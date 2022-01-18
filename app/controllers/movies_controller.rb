1acd1c75b29cf2f071526511239e03b966117fe7
PongthepNuchwet <jt061220@gmail.com>
2022-01-18 14:11:05 +0700

    start

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,67 @@
+class MoviesController < ApplicationController
+  def index
+    # @movies = Movie.all
+    @movies = Movie.all.order(:title)
+    @movies = @movies.sort_by { |value| value.title}
+    # Rails.logger.debug(@movies.class)
+  end
+
+  def new_record?
+    @new_record || false
+  end
+
+  def show
+    id = params[:id] 
+    
+    begin
+      @movie = Movie.find(id) 
+    rescue ActiveRecord::RecordNotFound => e
+      flash[:warning] = "no movie with the given ID could be found"
+      respond_to do |client_wants|
+        client_wants.html {  redirect_to movies_path  }
+        client_wants.xml  {  render :xml => @movie.to_xml    }
+      end
+    end
+  end
+
+  def new
+  end
+
+  def create
+    @movie = Movie.create!(movies_params)
+    flash[:notice] = "#{@movie.title} was successfully created."
+    respond_to do |client_wants|
+      client_wants.html {  redirect_to movie_path(@movie)  }
+      client_wants.xml  {  render :xml => @movie.to_xml    }
+    end
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+   
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update(movies_params)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    respond_to do |client_wants|
+      client_wants.html {  redirect_to movie_path(@movie)  }
+      client_wants.xml  {  render :xml => @movie.to_xml    }
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    respond_to do |client_wants|
+      client_wants.html {  redirect_to movies_path  }
+      client_wants.xml  {  render :xml => @movie.to_xml    }
+    end
+  end
+
+  private
+    def movies_params
+      params.require(:movie).permit(:title, :rating,:description, :release_date)
+    end
+end
